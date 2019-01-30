36d65e1c05ac947c8d13b1011614da6cabe5a372
metelo69 <metelo69@RobiLab325-26>
2019-01-29 19:42:22 -0500

    added kilometers and conversions as weel as changing area to circumference.

--- Source.cpp
+++ Source.cpp
@@ -6,7 +6,7 @@
 * Email: metelo69@students.rowan.edu
 ***********************************************
 * Purpose:
-*  This program calculates area of circle
+*  This program calculates circumference of circle
 ***********************************************/
 
 #include <iostream>
@@ -14,26 +14,47 @@
 using namespace std; 
 
 const  float PI = 3.14159;
+const  float CONV = 1.6;
 
 int main()
 {
 
-	float radius, area, circumference;
+	float miles, kilometers, meters, centimeters, millimeters, radius, circumference;
+	//Display purpose of the program
+	cout << " ******************************************" << endl;
+	cout << " This program takes in miles and converts them to kilometers" << endl;
+	cout << " ******************************************\n\n";
 	
+	//Get user input
+	cout << "Please input miles: ";
+	cin >> miles;
+
+	//compute conversion miles to kilometers
+	kilometers = miles / CONV;
+	meters = (miles / CONV) * 1000;
+	centimeters = (miles / CONV) * 100000;
+	millimeters = (miles / CONV) * 1000000;
+
+	//Display the kilometers 
+	cout << "kilometers = " << kilometers << "\n\n";
+	cout << "meters = " << meters << "\n\n"; 
+	cout << "centimeters = " << centimeters << "\n\n";
+	cout << "millimeters = " << millimeters << "\n\n";
+
 	//Display purpose of the program
 	cout << " ******************************************"<<endl;
-	cout << " This program takes in radius to compute area of circle" <<endl;
+	cout << " This program takes in radius to compute circumference of circle" <<endl;
 	cout << " ******************************************\n\n";
 	
 	//Get user input
 	cout << "Please input radius: ";
 	cin >> radius;
 	
-	//Compute area to PI*radius*radius
-	area = PI* radius *radius; 
+	//Compute circumference to PI*radius*2
+	circumference = PI*radius*2  ; 
 	
-	//Display the area 
-	cout << " The area is = " << area << "\n\n";
+	//Display the circumference 
+	cout << " The circumference is = " << circumference << "\n\n";
 	cout << "Program has ended"<<endl;
 
 	system("pause");
